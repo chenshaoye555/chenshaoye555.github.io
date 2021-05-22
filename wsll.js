3461638d113f6392f983bd5ad8cbf26e59d2ba81
chenshaoye555 <80017847+chenshaoye555@users.noreply.github.com>
2021-05-22 14:26:58 +0800

    Delete wsll.js

--- wsll.js
+++
@@ -1,8 +0,0 @@
-function showTopic(){
- var response = prompt("There is a game called cyberpunk__ ?");
-if(response=="2077")
-  {alert("Welcome to the city of night!");}
-else
-  {alert("Try again!");}
-}
-showTopic();
